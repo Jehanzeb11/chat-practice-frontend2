b244b77a8dcfb385bdc9bc7678e44e165c81a66b
jehanzeb <jehanzebsiddiqui287@gmail.com>
2024-07-31 12:12:44 +0500

    navbar added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='p-2 container flex items-center gap-20'>
+
+        <img src="https://cdn-ikpnlcj.nitrocdn.com/weiDAkCcNhepGRAFGfsIHxqnlniULSbJ/assets/images/optimized/rev-9e3a27d/fameitech.com/wp-content/uploads/2020/04/fame-itech1-png-ce7d2b56-1.png" alt="logo" className='w-56 p-2 '/>
+      
+
+<nav>
+
+<ul className='flex gap-8 font-medium text-base'>
+    <li className='hover:text-[#ff7502] hover:border-b-[2px] border-[#ff7502] cursor-pointer p-0 m-0 transition-all ease-in-out'>ABOUT US </li>
+    <li className='hover:text-[#ff7502] hover:border-b-[2px] border-[#ff7502] cursor-pointer p-0 m-0 transition-all ease-in-out'>LOGO DESIGN</li>
+    <li className='hover:text-[#ff7502] hover:border-b-[2px] border-[#ff7502] cursor-pointer p-0 m-0 transition-all ease-in-out'>WEBSITE DESIGN</li>
+    <li className='hover:text-[#ff7502] hover:border-b-[2px] border-[#ff7502] cursor-pointer p-0 m-0 transition-all ease-in-out'>VIDEO ANIMATION</li>
+    <li className='hover:text-[#ff7502] hover:border-b-[2px] border-[#ff7502] cursor-pointer p-0 m-0 transition-all ease-in-out'>OUR PACKAGES</li>
+    <li className='hover:text-[#ff7502] hover:border-b-[2px] border-[#ff7502] cursor-pointer p-0 m-0 transition-all ease-in-out'>CONTACT</li>
+</ul>
+
+</nav>
+
+    </header>
+  )
+}
+
+export default Navbar
